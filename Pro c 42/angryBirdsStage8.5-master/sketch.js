8a90d65cc804b91a3d02ce2fcd8ffd3594f6b1ff
FINGSHEEP <59056616+FINGSHEEP@users.noreply.github.com>
2020-05-08 12:16:21 +0530

    Add files via upload

---
+++ Pro c 42/angryBirdsStage8.5-master/sketch.js
@@ -0,0 +1,18 @@
+var player ; 
+
+function setup() {
+        canvas = createCanvas(600,600) ;
+        player  = new Player(100, 250, 250, 170, 330, 300);  ; 
+} 
+
+function draw() {
+        background(220) ; 
+        player.display() ;
+        player.keyPressed() ;
+  
+        
+
+
+
+}   
+
